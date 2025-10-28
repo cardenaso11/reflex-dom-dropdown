295a4f0a79f6170c5d8ac42f5446392bbd032158
card <cardenaso11@users.noreply.github.com>
2025-10-28 12:30:28 +0000

    Set  manually to make sure vscode doesn't clobber it, add obelisk dependency (probably unneeded currently)

--- project.nix
+++ project.nix
@@ -1,7 +1,9 @@
-{ config, nix-haskell-patches, ... }:
+{ config, nix-haskell-patches, lib, pkgs, ... }:
 
 let nix-thunk = config.importing.nix-thunk;
     deps = with nix-thunk; mapSubdirectories thunkSource ./deps;
+    # nixpkgs = import deps.nixpkgs {};
+    obelisk = (import deps.obelisk{}).command;
 
 in {
   imports = [
@@ -27,10 +29,46 @@ in {
       reflex-dom-lazy
     ];
     withHaddock = false;
-    withHoogle = false;
+    withHoogle = true;
     tools = {
       haskell-language-server = {};
     };
-    inputsFrom = [];
+
+    # The TMP thing is necessary for vscode devcontainer, which starts up a nix-shell, sets the env variable in that
+    # There's probably a way to do it without that
+    # The optional string bit is cut verbatim from hnix: https://github.com/input-output-hk/haskell.nix/blob/e34dc3262c41fee2eb1e076475705e3e5e4b1450/modules/shell.nix#L67
+    shellHook = lib.mkForce (
+      ''
+        export TMPDIR=/tmp
+        export TMP=/tmp
+        export TEMP=/tmp
+      ''
+      + lib.optionalString pkgs.stdenv.hostPlatform.isGhcjs ''
+        if [ -z "$EM_CACHE" ]; then
+          # Create a unique temporary directory using mktemp
+          EM_CACHE_DIR=$(mktemp -d -t emcache-ghcjs-XXXXXX)
+
+          # Copy the default Emscripten cache contents to the temporary directory
+          DEFAULT_EM_CACHE="${pkgs.pkgsBuildBuild.emscripten}/share/emscripten/cache"
+          if [ -d "$DEFAULT_EM_CACHE" ]; then
+            cp -r "$DEFAULT_EM_CACHE"/* "$EM_CACHE_DIR" 2>/dev/null || true
+            chmod -R u+w "$EM_CACHE_DIR"
+          fi
+
+          export EM_CACHE="$EM_CACHE_DIR"
+          echo "Set EM_CACHE to $EM_CACHE"
+        else
+          echo "EM_CACHE already set to $EM_CACHE"
+        fi
+      ''
+    );
+    inputsFrom = [(pkgs.mkShell {
+      packages = [
+        pkgs.nil
+        pkgs.direnv
+        pkgs.nix-direnv
+        obelisk
+      ];
+    })];
   };
 }
